61e8f966da53cf59b1565900608a682a1f0234cf
Damian Posener <750763+deains@users.noreply.github.com>
2020-01-25 09:52:20 +0000

    Add asgiref and channels

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ from setuptools import setup
 setup(
     name='racetime',
     install_requires=[
+        'asgiref>=3.2.3',
         'beautifulsoup4>=4.8.2,<4.9',
+        'channels>=2.4.0,<2.5',
         'Django>=3.0.1,<3.1',
         'django-recaptcha>=2.0.5,<2.1',
         'hashids>=1.2.0,<1.3',
