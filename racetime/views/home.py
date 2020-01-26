1be477861517b2e9b5fbf23e4fca5c495456966c
Damian Posener <750763+deains@users.noreply.github.com>
2020-01-26 10:05:55 +0000

    Update home view

--- racetime/views/home.py
+++ racetime/views/home.py
@@ -1,15 +1,21 @@
 from django.db.models import Count, Q
 from django.views import generic
 
+from .base import UserMixin
 from ..models import Category, RaceStates
 
 
-class Home(generic.TemplateView):
+class Home(UserMixin, generic.TemplateView):
     template_name = 'racetime/home.html'
+
+    @property
+    def show_recordable(self):
+        return self.user.is_superuser
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({
+            'show_recordable': self.show_recordable,
             'categories': self.categories(),
         })
         return context
@@ -21,11 +27,25 @@ class Home(generic.TemplateView):
                 expression='race__id',
                 filter=Q(race__state__in=[c.value for c in RaceStates.current]),
             ),
-            total_race_count=Count(expression='race__id'),
+            finished_race_count=Count(
+                expression='race__id',
+                filter=Q(race__state=RaceStates.finished.value),
+            ),
         )
+        if self.show_recordable:
+            queryset = queryset.annotate(
+                recordable_race_count=Count(
+                    expression='race__id',
+                    filter=Q(
+                        race__recordable=True,
+                        race__recorded=False,
+                        race__state=RaceStates.finished.value,
+                    ),
+                )
+            )
         queryset = queryset.order_by(
             '-current_race_count',
-            '-total_race_count',
+            '-finished_race_count',
             'name',
         )
         return queryset.all()
