9e748e774c2e708c500116e794cbb5b88a10b956
Damian Posener <750763+deains@users.noreply.github.com>
2020-01-30 19:53:22 +0000

    Update middleware and URLs

--- racetime/routing.py
+++ racetime/routing.py
@@ -3,13 +3,17 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 
 from . import consumers
+from .middleware import OAuth2TokenMiddleware
 
 urlpatterns = [
-    path('ws/chat/<str:race>', consumers.ChatConsumer, name='race_chat'),
+    path('ws/chat/<str:race>', consumers.ChatConsumer),
+    path('ws/race/<str:race>', consumers.ChatConsumer, name='race_chat'),
 ]
 
+MiddlewareStack = lambda inner: OAuth2TokenMiddleware(AuthMiddlewareStack(inner))
+
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
+    'websocket': MiddlewareStack(
         URLRouter(urlpatterns)
     ),
 })
