e66659d7da9dc535140660cbb4115457104d5ae2
Damian Posener <750763+deains@users.noreply.github.com>
2020-02-06 17:44:22 +0000

    Management URLs are now dev only

--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
+from oauth2_provider.urls import management_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('o/', include((management_urlpatterns, 'oauth2_provider'), namespace='oauth2_provider')),
     path('', include('racetime.urls')),
 ]
