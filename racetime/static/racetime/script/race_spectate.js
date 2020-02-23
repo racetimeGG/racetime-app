4633de180af8872aefc7f2b046d5d763ddc1ecac
Damian Posener <750763+deains@users.noreply.github.com>
2020-02-23 14:57:20 +0000

    Add select all button

--- racetime/static/racetime/script/race_spectate.js
+++ racetime/static/racetime/script/race_spectate.js
@@ -1,12 +1,19 @@
 $(function() {
     $(document).on('click', '.race-streams .visibility', function() {
         var $li = $(this).closest('li');
-        $li.toggleClass('playing');
+        $li.toggleClass('playing not-playing');
         if ($li.hasClass('playing')) {
             $('<iframe allowfullscreen>').attr('src', $li.data('embed-uri'))
                 .appendTo($li.find('.player'));
         } else {
             $li.find('.player iframe').remove();
+        }
+    });
+    $(document).on('click', '.race-stream-control .open-all', function() {
+        if ($('.race-streams .not-playing').length) {
+            $('.race-streams .not-playing .visibility').trigger('click');
+        } else {
+            $('.race-streams .playing .visibility').trigger('click');
         }
     });
     $(document).on('click', '.race-stream-control .streamctl', function() {
