cf24d3b50d20b532fa79cc856627d80f0a8ad92f
Damian Posener <750763+deains@users.noreply.github.com>
2020-04-03 20:17:34 +0100

    Add handy redirect

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic import RedirectView
 from django.urls import path, include, re_path
 from django.views.static import serve
 from oauth2_provider.urls import management_urlpatterns
 
 urlpatterns = [
+    path('admin', RedirectView.as_view(url='admin/', permanent=True)),
     path('admin/', admin.site.urls),
     re_path('^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('o/', include((management_urlpatterns, 'oauth2_provider'), namespace='oauth2_provider')),
