3d919b7f185981d75faf56cfedd3bf152f17be86
Damian Posener <750763+deains@users.noreply.github.com>
2020-05-03 14:11:16 +0100

    Update filtering again

--- racetime/static/racetime/script/home.js
+++ racetime/static/racetime/script/home.js
@@ -1,11 +1,14 @@
 $(function() {
     var homeFilter = function() {
-        var value = $(this).val().toLowerCase().replace('"', '');
+        var value = $(this).val().toLowerCase().replace(/"/g, '');
         var $all = $('.home-categories').children(':not(.request-category, [data-slug="misc"])');
+        var $misc = $('.home-categories').children('.request-category, [data-slug="misc"]');
         if (value) {
-            $all.hide().filter('[data-search*="' + value + '"]').show();
+            var $found = $all.hide().filter('[data-search*="' + value + '"]').show();
+            $misc.not($found)[$found.length === 0 ? 'show' : 'hide']();
         } else {
             $all.show();
+            $misc.show();
         }
     };
     $(document).on('change input keyup', '.home-filter', homeFilter);
