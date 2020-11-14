301ec715b5ae873d0fde212457283920538ba10f
Damian Posener <750763+deains@users.noreply.github.com>
2020-11-14 13:17:14 +0000

    Freeze versions for compatibility

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 setup(
     name='racetime',
     install_requires=[
-        'asgiref>=3.2.3',
+        'asgiref>=3.2.3,<3.3',
         'beautifulsoup4>=4.8.2,<4.10',
         'channels>=2.4.0,<2.5',
-        'Django>=3.0.1,<3.2',
+        'Django>=3.0.1,<3.1',
         'django-admin-list-filter-dropdown',
         'django-oauth-toolkit>=1.2.0,<1.4',
         'django-recaptcha>=2.0.5,<2.1',
